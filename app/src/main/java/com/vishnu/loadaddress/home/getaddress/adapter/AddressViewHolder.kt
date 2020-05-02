72162a5945a1884bcdd7c1a658fb2e4a831ec4fb
vishnubenny <vishnubenny@gmail.com>
2020-05-02 22:35:48 +0530

    UI update

--- app/src/main/java/com/vishnu/loadaddress/home/getaddress/adapter/AddressViewHolder.kt
+++ app/src/main/java/com/vishnu/loadaddress/home/getaddress/adapter/AddressViewHolder.kt
@@ -3,9 +3,10 @@ package com.vishnu.loadaddress.home.getaddress.adapter
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.vishnu.core.getaddress.model.AddressItem
+import kotlinx.android.synthetic.main.row_address_item.view.*
 
-class AddressViewHolder(private val itemView: View) : RecyclerView.ViewHolder(itemView) {
+class AddressViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
     fun bindData(addressItem: AddressItem) {
-
+        view.txtVwAddress.text = addressItem.addressString
     }
 }
