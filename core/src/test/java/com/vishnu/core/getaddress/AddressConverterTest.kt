9d78b4881b79dff6d4fba046076764f4311413da
vishnubenny <vishnubenny@gmail.com>
2020-05-03 09:16:58 +0530

    Add UT

--- core/src/test/java/com/vishnu/core/getaddress/AddressConverterTest.kt
+++ core/src/test/java/com/vishnu/core/getaddress/AddressConverterTest.kt
@@ -15,12 +15,14 @@ class AddressConverterTest {
     private val apiAddress = Mockito.mock(ApiAddress::class.java)
     private val apiAddressData = Mockito.mock(ApiAddressData::class.java)
     private val apiAddressItem = Mockito.mock(ApiAddressItem::class.java)
+    private val apiEmptyAddressItem = Mockito.mock(ApiAddressItem::class.java)
     private lateinit var converter: AddressConverter
 
     @Before
     fun setUp() {
         whenever(apiAddress.data).thenReturn(apiAddressData)
         whenever(apiAddressItem.addressString).thenReturn("address string")
+        whenever(apiEmptyAddressItem.addressString).thenReturn("")
         converter = AddressConverter()
     }
 
@@ -37,6 +39,23 @@ class AddressConverterTest {
     @Test
     fun `should convert to Address data list with proper value if the API having data`() {
         whenever(apiAddressData.addressList).thenReturn(listOf(apiAddressItem, apiAddressItem))
+        val addressItem = AddressItem("address string")
+        val expected = Address(listOf(addressItem, addressItem))
+
+        val converted = converter.apply(apiAddress)
+
+        assertThat(converted).isEqualTo(expected)
+    }
+
+    @Test
+    fun `converter should not add empty address string to the Address data list`() {
+        whenever(apiAddressData.addressList).thenReturn(
+            listOf(
+                apiAddressItem,
+                apiEmptyAddressItem,
+                apiAddressItem
+            )
+        )
         val addressItem = AddressItem("address string")
         val expected = Address(listOf(addressItem, addressItem))
 
