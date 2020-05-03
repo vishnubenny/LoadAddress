72d8ba88249d19b75e2082fd280795e51c669b6e
vishnubenny <vishnubenny@gmail.com>
2020-05-03 09:05:02 +0530

    Add UT

--- core/src/test/java/com/vishnu/core/getaddress/AddressConverterTest.kt
+++ core/src/test/java/com/vishnu/core/getaddress/AddressConverterTest.kt
@@ -2,9 +2,7 @@ package com.vishnu.core.getaddress
 
 import com.google.common.truth.Truth.assertThat
 import com.nhaarman.mockitokotlin2.whenever
-import com.vishnu.core.getaddress.model.Address
-import com.vishnu.core.getaddress.model.ApiAddress
-import com.vishnu.core.getaddress.model.ApiAddressData
+import com.vishnu.core.getaddress.model.*
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -16,18 +14,31 @@ class AddressConverterTest {
 
     private val apiAddress = Mockito.mock(ApiAddress::class.java)
     private val apiAddressData = Mockito.mock(ApiAddressData::class.java)
+    private val apiAddressItem = Mockito.mock(ApiAddressItem::class.java)
     private lateinit var converter: AddressConverter
 
     @Before
     fun setUp() {
+        whenever(apiAddress.data).thenReturn(apiAddressData)
+        whenever(apiAddressItem.addressString).thenReturn("address string")
         converter = AddressConverter()
     }
 
     @Test
     fun `should convert to Address data with empty list if the API have no address list`() {
-        whenever(apiAddress.data).thenReturn(apiAddressData)
         whenever(apiAddressData.addressList).thenReturn(emptyList())
         val expected = Address(emptyList())
+
+        val converted = converter.apply(apiAddress)
+
+        assertThat(converted).isEqualTo(expected)
+    }
+
+    @Test
+    fun `should convert to Address data list with proper value if the API having data`() {
+        whenever(apiAddressData.addressList).thenReturn(listOf(apiAddressItem, apiAddressItem))
+        val addressItem = AddressItem("address string")
+        val expected = Address(listOf(addressItem, addressItem))
 
         val converted = converter.apply(apiAddress)
 
